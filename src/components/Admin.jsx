0d3f9a6a7429f1c700c73d2c2f636e07c0bb3ea5
Marc Davies <marcdaviesriot@gmail.com>
2019-07-12 09:22:59 -0700

    add curentRouterPath propType

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import TicketList from './TicketList';
 
-function Admin(){
+function Admin(props){
   return (
     <div>
       <h2>Admin</h2>
-        <TicketList
-          ticketList={props.ticketList}
-          currentRouterPath={props.currentRouterPath} />
+      <TicketList
+        ticketList={props.ticketList}
+        currentRouterPath={props.currentRouterPath} />
     </div>
   );
 }
