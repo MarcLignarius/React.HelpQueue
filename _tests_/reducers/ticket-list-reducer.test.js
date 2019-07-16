26d9afef8f80a2d32a8a93b702dd9fea7a9abb3e
Marc Davies <marcdaviesriot@gmail.com>
2019-07-15 23:44:31 -0700

    add and fail test for 'Should return default state if no action type is recognized'

--- _tests_/reducers/ticket-list-reducer.test.js
+++ _tests_/reducers/ticket-list-reducer.test.js
@@ -1,9 +1,9 @@
-import ticketListReducer from './../src/reducers/ticket-list-reducer';
+import ticketListReducer from './../../src/reducers/ticket-list-reducer';
 
 describe('ticketListReducer', () => {
 
-  test('Temporary dummy test: two plus two is four', () => {
-      expect(2 + 2).toBe(4);
-    });
+  test('Should return default state if no action type is recognized', () => {
+    expect(ticketListReducer({}, { type: null })).toEqual({});
+  });
 
 });
