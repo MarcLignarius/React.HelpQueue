154fced5d7c47fbe618fcd017780be4e622662b9
Marc Davies <marcdaviesriot@gmail.com>
2019-07-16 00:32:26 -0700

    add ticket details to action payload and dispatch action

--- src/components/NewTicketForm.jsx
+++ src/components/NewTicketForm.jsx
@@ -12,8 +12,14 @@ function NewTicketForm(props){
   const { dispatch } = props;
   event.preventDefault();
   const action = {
-    type: 'ADD_TICKET'
+    type: 'ADD_TICKET',
+    id: null,
+    names: _names.value,
+    location: _location.value,
+    issue: _issue.value,
+    timeOpen: new Moment()
   };
+  dispatch(action);
   props.onNewTicketCreation({names: _names.value, location: _location.value, issue: _issue.value, id: v4(), timeOpen: new Moment()});
   _names.value = '';
   _location.value = '';
