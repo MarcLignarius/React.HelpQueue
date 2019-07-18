64822606c5e6b24098378004a970925c759c3d27
Marc Davies <marcdaviesriot@gmail.com>
2019-07-18 01:46:28 -0700

    fail and pass test for Should add freshly-calculated Moment-formatted wait time to ticket entry

--- src/reducers/ticket-list-reducer.js
+++ src/reducers/ticket-list-reducer.js
@@ -3,14 +3,15 @@ export default (state = {}, action) => {
   const { names, location, issue, timeOpen, id, formattedWaitTime } = action;
 
   switch (action.type) {
-  case 'ADD_TICKET':
+    case 'ADD_TICKET':
     newState = Object.assign({}, state, {
       [id]: {
         names: names,
         location: location,
         issue: issue,
         timeOpen: timeOpen,
-        id: id
+        id: id,
+        formattedWaitTime: formattedWaitTime
       }
     });
     return newState;
