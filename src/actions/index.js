cb2851cf83a3c369a2dc446c89f04dec6f46e729
Student <example@example.com>
2019-07-24 16:08:44 -0700

    import middleware to manage async actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import constants from './../constants';
 const { firebaseConfig } = constants;
-import firebase from 'firebase';
+import Firebase from 'firebase';
 
 firebase.initializeApp(firebaseConfig);
 const tickets = firebase.database().ref('tickets');
